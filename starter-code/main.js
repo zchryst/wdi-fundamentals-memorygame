8b6dd5065908aa3f4196c7ccf899b7ce393b6ddb
Zach Chrystall <z.chrystall@gmail.com>
2017-01-26 11:53:01 +1300

    added game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,9 @@ var cardOne = 'queen'
 var cardTwo = 'queen'
 var cardThree = 'king'
 var cardFour = 'king'
+
+if (cardThree === cardFour) {
+	alert("You found a match!")
+} else {
+	alert("Sorry try again!")
+}
