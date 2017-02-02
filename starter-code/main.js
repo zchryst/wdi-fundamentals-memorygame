69b06647240da9237543ff6c851222da78de83af
Zach Chrystall <z.chrystall@gmail.com>
2017-02-03 09:21:41 +1100

    Added JS to create cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,8 +5,19 @@ var cardTwo = 'queen'
 var cardThree = 'king'
 var cardFour = 'king'
 
-if (cardThree === cardFour) {
+/*if (cardThree === cardFour) {
 	alert("You found a match!")
 } else {
 	alert("Sorry try again!")
+}*/
+
+var createCards = function() {
+	for (var i = 1; i <= 4; i++) {
+		var myBoard = document.getElementById('game-board');
+		var newCard = document.createElement('div');
+		newCard.className = 'card';
+		myBoard.appendChild(newCard);
+	}
 }
+
+createCards();
